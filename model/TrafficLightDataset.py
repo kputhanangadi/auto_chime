cc582c307bcc3d634e645546641ce9bae17d5f93
im-calvin <kelvinwong0519@gmail.com>
2023-07-24 18:47:51 -0700

    datasets working

--- model/TrafficLightDataset.py
+++ model/TrafficLightDataset.py
@@ -3,19 +3,27 @@ from PIL import Image
 import torch
 from torch.utils.data import Dataset
 
+
 class TrafficLightDataset(Dataset):
     def __init__(self, root_dir, transform=None):
         self.root_dir = root_dir
         self.transform = transform
-        self.image_filenames = [os.path.join(root_dir, label, f) for label in os.listdir(root_dir) for f in os.listdir(os.path.join(root_dir, label)) if f.endswith('.jpg')]
+        self.image_filenames = [
+            os.path.join(root_dir, label, f)
+            for label in os.listdir(root_dir)
+            for f in os.listdir(os.path.join(root_dir, label))
+            if f.endswith(".jpg")
+        ]
+        self.label_map = {"0_green": 0, "1_yellow": 1, "2_red": 2, "3_not": 3}
 
     def __len__(self):
         return len(self.image_filenames)
 
     def __getitem__(self, idx):
         img_path = self.image_filenames[idx]
-        image = Image.open(img_path).convert('RGB')
-        label = int(os.path.basename(os.path.dirname(img_path)))
+        image = Image.open(img_path).convert("RGB")
+        label = os.path.basename(os.path.dirname(img_path))
+        label = self.label_map[label]
         if self.transform:
             image = self.transform(image)
         return image, label
